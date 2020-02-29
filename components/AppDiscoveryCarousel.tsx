fe7d05a1dcf1024650d4f37f856c2225f0eca29b
Pham Hieu <phamhieu1998@gmail.com>
2020-02-29 19:23:52 +0800

    update carousel item

--- components/AppDiscoveryCarousel.tsx
+++ components/AppDiscoveryCarousel.tsx
@@ -10,16 +10,20 @@ type Props = {
     image: string
   },
   url: string
+  actionText?: string,
 }
 
 const AppDiscoveryCarouselItem: React.FunctionComponent<Props> = ({
   title = "",
   description = "",
   author = { name: "", image: "" },
-  url = ""
+  url = "",
+  actionText = undefined
 }) => {
   const { name: authorName, image: authorImage } = author;
-
+  const formatDesc = description.split('  ').map((item, i) => {
+    return <p key={i}>{item}</p>
+  });
   return (
     <div className="app-discovery-carousel-item">
       <div className="app-discovery-carousel-item__inner">
@@ -28,8 +32,10 @@ const AppDiscoveryCarouselItem: React.FunctionComponent<Props> = ({
           <img src={authorImage} />
         </div>
         <h1>{title}</h1>
-        <p>{description}</p>
-        <a className="btn" href={url}>Check out</a>
+        {formatDesc}
+        <p className="action-container">
+          <a className="btn" href={url}>{actionText ? actionText : "Check out"}</a>
+        </p>
       </div>
     </div>
   )
@@ -49,41 +55,38 @@ const AppDiscoveryCarousel: React.FunctionComponent = () => {
       <Slider {...slickSettings}>
         <AppDiscoveryCarouselItem
           title="Arweave web archive"
-          description="30% of web links break within two years. The traditional web is failing you. 
-
-          Archive your favourite sites and sources, ensuring they exist forever."
+          description="30% of web links break within two years. The traditional web is failing you.  Archive your favourite sites and sources, ensuring they exist forever."
           url="https://chrome.google.com/webstore/detail/arweave/iplppiggblloelhoglpmkmbinggcaaoc"
           author={{ name: "Jane Doe", image: "https://via.placeholder.com/100x100.png" }}
+          actionText="Archive Now"
         />
         <AppDiscoveryCarouselItem
           title="Feedweave"
-          description="A decentralised, trustless blogging platform that knows who should own your data. 
-
-          Spoiler: It is you."
+          description="A decentralised, trustless blogging platform that knows who should own your data.  Spoiler: It is you."
           url="https://google.com"
           author={{ name: "@literature", image: "https://via.placeholder.com/100x100.png" }}
+          actionText="Read More"
         />
         <AppDiscoveryCarouselItem
           title="Weavemail"
-          description="Big-tech has taken control of email. We no longer have ownership of our own identities. 
-
-          Weavemail is a mail network that with integrity. It will never sell your data, show adverts, or violate your privacy."
+          description="Big-tech has taken control of email. We no longer have ownership of our own identities.  Weavemail is a mail network that with integrity. It will never sell your data, show adverts, or violate your privacy."
           url="https://google.com"
           author={{ name: "May Doe", image: "https://via.placeholder.com/100x100.png" }}
+          actionText="Get Your Address"
         />
         <AppDiscoveryCarouselItem
           title="Ownerless communities"
-          description="Content moderation policies frequently rip online communities apart, or allow trolls to run rampant. 
-
-          Permaweb apps solve these problems by being ownerless. Their moderation is transparent and described in openly accessible code, enabling a new breed of decentralised online communities."
+          description="Content moderation policies frequently rip online communities apart, or allow trolls to run rampant.  Permaweb apps solve these problems by being ownerless. Their moderation is transparent and described in openly accessible code, enabling a new breed of decentralised online communities."
           url="https://google.com"
           author={{ name: "@aokisok", image: "https://via.placeholder.com/100x100.png" }}
+          actionText="Join the Conversation"
         />
         <AppDiscoveryCarouselItem
           title="Explore the permaweb"
           description="The permaweb already hosts hundreds of immutable and decentralised apps. Explore a selection of them here."
           url="https://google.com"
           author={{ name: "@cedriking", image: "https://via.placeholder.com/100x100.png" }}
+          actionText="Check Them Out [-> Arweave Apps]"
         />
       </Slider>
     </div >
