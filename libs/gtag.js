a3024e437c29fea832afd7a7bfc31d8679dd96dc
Pham Hieu <phamhieu1998@gmail.com>
2020-03-03 22:40:00 +0800

    block gtag event until user allow cookies consent

--- libs/gtag.js
+++ libs/gtag.js
@@ -1,5 +1,10 @@
+import Cookies from "js-cookie"
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = url => {
+  const allowCookies = Cookies.get('does_allow_cookies_usage');
+  if (!allowCookies || allowCookies !== "true") return;
+
   window.gtag('config', process.env.GA_MEASUREMENT_ID, {
     page_path: url,
   })
@@ -7,6 +12,9 @@ export const pageview = url => {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
+  const allowCookies = Cookies.get('does_allow_cookies_usage');
+  if (!allowCookies || allowCookies !== "true") return;
+
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
