5172b361d747f8db4bfbf3123c65991073e4981b
Lili <li.saengsanthitam@gmail.com>
2020-03-04 10:51:07 +0100

    Add dots

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   return (
     <Layout withAos withHexagonBg className="homepage">
       <CardLinkSection aosConfig={{ "data-aos": "fade-up", "data-aos-easing": "linear", "data-aos-duration": "500" }} 
-        title="Store data, permanently"
+        title="Store data, permanently."
         desc="Arweave enables you  
         to store documents and applications forever."
         url="/#arweave-intro"
@@ -19,13 +19,13 @@ const Home: NextPage = () => {
       <a className="anchor" id="arweave-intro"/>
       <CardLinkSection aosConfig={{ "data-aos": "fade-up", "data-aos-easing": "linear", "data-aos-duration": "500" }} 
         align="left" 
-        title="Arweave makes information permanence sustainable"
+        title="Arweave makes information permanence sustainable."
         desc="Arweave is a new type of storage that backs data with sustainable and perpetual endowments, allowing users and developers to truly store data forever for the very first time. How? 
         As a collectively owned hard drive that never forgets, Arweave allows us to remember and preserve valuable information, apps, and history indefinitely. By preserving history, it prevents others from rewriting it."
         />
       <CardLinkSection aosConfig={{ "data-aos": "fade-up", "data-aos-easing": "linear", "data-aos-duration": "500" }} 
         align="right" 
-        title="Welcome to the permaweb"
+        title="Welcome to the permaweb."
         desc="On top of the Arweave network lives the permaweb: a global, community-owned web that anyone can contribute to or get paid to maintain. 
         The permaweb looks just like the normal web (you are viewing it right now), but all of its content – from images to full web apps –is permanent, retrieved quickly, and decentralized – forever. Just as the first web connected people over vast distances, the permaweb connects people over extremely long periods of time. 
         No more 404s. No more stealth edits. No more web apps that decline in quality."
